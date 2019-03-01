a9c53ea191497ed11a28398abdcb772145177d80
Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
2019-03-01 18:23:07 +0100

    Přidán skript který snadno otevře všechny zdrojáky v projektu

---
+++ open_ide.sh
@@ -0,0 +1,4 @@
+files_h=$(find . -path ./build -prune -o -path ./junk -prune -o -path ./.git -prune -o -name *.h -print)
+files_c=$(find . -path ./build -prune -o -path ./junk -prune -o -path ./.git -prune -o -name *.c -print)
+
+geany -i $files_c $files_h CMakeLists.txt &
