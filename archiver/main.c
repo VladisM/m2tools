43368a939a0eee83b5318d72f72b157f86093b5b
Vladislav <v.mlejnecky@seznam.cz>
2019-03-29 09:29:09 +0100

    dddd

--- archiver/main.c
+++ archiver/main.c
@@ -22,7 +22,8 @@ Arguments:\n\
     -o --output         Output filename for library.\n\
     -c --create         Create an archive from given object files.\n\
        --list           Print object files in archive.\n\
-       --extract        Extract all object files from archive into current dir.\n"
+       --extract        Extract all object files from archive into current dir.\n\
+       --verbose        Be verbose when extracting. Only with --extract\n"
 
 static void print_version(void);
 static void print_help(char *cmd_name);
@@ -46,6 +47,7 @@ typedef struct{
     unsigned input_files_count;
     unsigned input_files_len;
     action_t action;
+    int verbose;
 }settings_t;
 
 settings_t settings;
@@ -65,6 +67,7 @@ int main(int argc, char **argv){
     settings.input_files_len = 1;
     settings.out_file_name = NULL;
     settings.action = ACTION_NOT_SPECIFIED;
+    settings.verbose = 0;
 
     arg_parse(argc, argv);
 
@@ -172,6 +175,7 @@ static void arg_parse(int argc, char* argv[]){
             }
             else{
                 fprintf(stderr, "You cannot set this with this action!");
+                exit(EXIT_FAILURE);
             }
         }
         else if(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--create") == 0){
@@ -194,6 +198,19 @@ static void arg_parse(int argc, char* argv[]){
                 exit(EXIT_FAILURE);
             }
             settings.action = EXTRACT_ARCHIVE;
+        }
+        else if(strcmp(argv[i], "--verbose") == 0){
+            if(settings.action == ACTION_NOT_SPECIFIED){
+                fprintf(stderr, "You have to specify action before anything else!\n");
+                exit(EXIT_FAILURE);
+            }
+            else if(settings.action == EXTRACT_ARCHIVE){
+                settings.verbose = 1;
+            }
+            else{
+                fprintf(stderr, "You cannot set this with this action!");
+                exit(EXIT_FAILURE);
+            }
         }
         else{
             if(argv[i][0] == '-'){
@@ -261,12 +278,10 @@ static void create_library(char *out_file, char **input_files, unsigned file_cou
 
         if(append_objfile_to_sl(new_obj, new_lib) != 0){
             fprintf(stderr, "Failed to append object file into library! sllib errno: %d\n", get_sllib_errno());
-            //~ free_object_file(new_obj);
             free_sl(new_lib);
             exit(EXIT_FAILURE);
         }
 
-        //~ free_object_file(new_obj);
     }
 
     if(sl_write(out_file, new_lib) != 0){
@@ -279,14 +294,43 @@ static void create_library(char *out_file, char **input_files, unsigned file_cou
 }
 
 static void extract_library(char *input_archive){
-    fprintf(stderr, "Action not implemented yet!\n");
-    exit(EXIT_FAILURE);
+    static_library_t *loaded_lib = NULL;
+
+    //fail if wrong arch
+    if(sl_load(input_archive, &loaded_lib) != 0){
+        fprintf(stderr, "Failed to load library '%s'!\n", input_archive);
+        exit(EXIT_FAILURE);
+    }
+
+    if(settings.verbose == 1){
+        printf("Archive %s:\n", loaded_lib->library_name);
+    }
+
+    obj_file_t *head_obj_file = loaded_lib->first_obj;
+
+    while(head_obj_file != NULL){
+
+        if(settings.verbose == 1){
+            printf(" %s\n", head_obj_file->object_file_name);
+        }
+
+        if(obj_write(head_obj_file->object_file_name, head_obj_file) != 0){
+            fprintf(stderr, "Failed to create object file '%s'! objlib errno: %d", head_obj_file->object_file_name, get_objlib_errno());
+            free_sl(loaded_lib);
+            exit(EXIT_FAILURE);
+        }
+
+        head_obj_file = head_obj_file->next;
+    }
+
+    free_sl(loaded_lib);
 }
 
 static void list_library(char *input_archive){
 
     static_library_t *loaded_lib = NULL;
 
+    //fail if wrong arch
     if(sl_load(input_archive, &loaded_lib) != 0){
         fprintf(stderr, "Failed to load library '%s'!\n", input_archive);
         exit(EXIT_FAILURE);
