54924ee685c5ff49e473645787d7e53fb350d69d
Vladislav <v.mlejnecky@seznam.cz>
2019-04-01 14:14:13 +0200

    update cmakes

--- open_ide.sh
+++ open_ide.sh
@@ -1,4 +1,5 @@
 files_h=$(find . -path ./build -prune -o -path ./junk -prune -o -path ./.git -prune -o -name *.h -print)
 files_c=$(find . -path ./build -prune -o -path ./junk -prune -o -path ./.git -prune -o -name *.c -print)
+files_txt=$(find . -path ./build -prune -o -path ./junk -prune -o -path ./.git -prune -o -name *.txt -print)
 
-geany -i $files_c $files_h CMakeLists.txt TODO &
+geany -i $files_c $files_h $files_txt TODO &
