9dc0d1b497671f856d29b73e6329e037073c63d7
Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
2019-06-14 17:03:24 +0200

    Přidány komentáře k objlib

--- objlib/obj.h
+++ objlib/obj.h
@@ -5,10 +5,9 @@
  * @file obj.h
  *
  * @brief Library for dealing with task related with object files.
+ *
+ * @todo Add example of ussage and describe obj group more.
  */
-
-//TODO: Dopsat pořádné komentáře
-//TODO: Napsat example použití do dokumentace
 
 /**
  * @defgroup obj Object files library
@@ -105,54 +104,227 @@ typedef struct section_s{
     data_symbol_t *data_last;           /**< @brief Pointer to the last element of data stream. */
 }section_t;
 
+
+/**
+ * @brief Struct to hold one complete object file.
+ *
+ * Object file have some properties like target_arch_name and object_file_name.
+ * Also it is supposed to be connected into double linked list of object files.
+ * This feature can be used by linker or archiver.
+ *
+ * Object file consist of list of sections connected into (well ... again) double
+ * linked list (yeah, I love double linked lists).
+ */
 typedef struct obj_file_s{
-    char *object_file_name;
-    char *target_arch_name;
-    section_t *first_section;
-    section_t *last_section;
-    struct obj_file_s *next;
-    struct obj_file_s *prev;
+    char *object_file_name;     /**< @brief Name of this object file. Usually same as filename.*/
+    char *target_arch_name;     /**< @brief Name of target architecture, library have to be compiled for same target. */
+    section_t *first_section;   /**< @brief Pointer to first section in list. */
+    section_t *last_section;    /**< @brief Pointer to last section in the list. */
+    struct obj_file_s *next;    /**< @brief Pointer to the next object file. */
+    struct obj_file_s *prev;    /**< @brief Pointer to the last object file in the list. */
 }obj_file_t;
 
-//výčtový typ pro chyby vzniknouvší v modulu
+/**
+ * @brief Enum type for errors in objlib.
+ */
 typedef enum{
-    OBJRET_OK = 0,
-    OBJRET_BROKEN_OBJ,
-    OBJRET_NULL_PTR,
-    OBJRET_MALLOC_FAIL,
-    OBJRET_OBJ_EXIST_ALREADY,
-    OBJRET_BROKEN_SECTION,
-    OBJRET_FOPEN_ERROR,
-    OBJRET_INTERNAL_ERR,
-    OBJRET_BROKEN_FILE,
-    OBJRET_SECTION_EXIST_ALREADY,
-    OBJRET_WRONG_ARCH,
-    OBJRET_WRONG_ARG
+    OBJRET_OK = 0,                      /**< @brief Everything is OK. :) */
+    OBJRET_BROKEN_OBJ,                  /**< @brief Object file is broken, excepted something different from what we get. */
+    OBJRET_NULL_PTR,                    /**< @brief Pointer in function arg is NULL. */
+    OBJRET_MALLOC_FAIL,                 /**< @brief Failed to allocate memory. */
+    OBJRET_BROKEN_SECTION,              /**< @brief Section is incosistent.*/
+    OBJRET_FOPEN_ERROR,                 /**< @brief Failed to open file. */
+    OBJRET_INTERNAL_ERR,                /**< @brief Not specified error, may occur in underlying library. */
+    OBJRET_BROKEN_FILE,                 /**< @brief Structure of the object file in corrupted. */
+    OBJRET_SECTION_EXIST_ALREADY,       /**< @brief This section already exist in object file. You should connet them before adding it.*/
+    OBJRET_WRONG_ARCH,                  /**< @brief Object file is intended for another target than library. */
+    OBJRET_WRONG_ARG                    /**< @brief Given arguments for fuction are in wrong format. Maybe some pointer have to be NULL and it doesn't? */
 }obj_file_err_t;
 
-//funkce pro práci s chybama v modulu
+/**
+ * @brief Clear variable holding error code.
+ */
 void clear_objlib_errno(void);
+
+/**
+ * @brief Get error code.
+ *
+ * @return One of the obj_file_err_t value.
+ */
 obj_file_err_t get_objlib_errno(void);
 
-//funkce pro vyčištění dynamické paměti
+/**
+ * @brief Clear object file.
+ *
+ * Object file is full of pointer, pointing to pointeres, pointing to another pointeres,
+ * that finally, point to chars arrays, witch is ... pointers. This all is dynamically
+ * allocated memory and you have to free it, you this function to do so. :)
+ *
+ * @param o Pointer to object file to clean.
+ */
 void free_object_file(obj_file_t *o);
 
-//funkce pro práci se souborem
+/**
+ * @brief Load object file from string buffer into structure.
+ *
+ * @param s String that hold content of object file.
+ * @param o Pointer for struct where object file will be loaded into.
+ *
+ * @return 0 if ok, -1 if fail
+ *
+ * @note param o have to point to NULL ( *o == NULL ), correct struct will be
+ * created by calling this function.
+ *
+ * @note This function is intended mainly for using in linker or archiver. Object file is
+ * generaly only text file in special format, and therefore can be stored in string.
+ */
 int obj_load_from_string(char *s, obj_file_t **o);
+
+/**
+ * @brief Load object file from regular file on the disk.
+ *
+ * @param filename Path with file that will be loaded.
+ * @param o Pointer for struct where object file will be loaded into.
+ *
+ * @return 0 if ok, -1 if fail
+ *
+ * @note param o have to point to NULL ( *o == NULL ), correct struct will be
+ * created by calling this function.
+ */
 int obj_load_from_file(char *filename, obj_file_t **o);
+
+/**
+ * @brief Write object file into string.
+ *
+ * @param s Pointer to string where object will be printed out.
+ * @param o Pointer to object that will be written out.
+ *
+ * @return 0 if ok, -1 if fail
+ *
+ * @note param s have to point to NULL ( *s == NULL ), memory will be allocated by
+ * this function. Don't forget to free it! ;)
+ *
+ * @note This function is intended mainly for using in linker or archiver. Object file is
+ * generaly only text file in special format, and therefore can be stored in string.
+ */
 int obj_write_to_string(char **s, obj_file_t *o);
+
+/**
+ * @brief Write object file into regular file.
+ *
+ * @param filename Path with file name where object file will be written to.
+ * @param o Pointer to object that will be written out.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int obj_write_to_file(char *filename, obj_file_t *o);
 
-//vytvoří nové objekty
+/**
+ * @brief Create new structure for object file.
+ *
+ * @param object_file_name String with name of object file.
+ * @param o Pointer to pointer to object struct.
+ *
+ * @note o have to be pointing to NULL. ( *o == NULL ). Don't forget ot
+ * free this struct by calling free_object_file().
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int new_obj(char * object_file_name, obj_file_t **o);
+
+/**
+ * @brief Create new section.
+ *
+ * @param secion_name Name of the section that will be created.
+ * @param s Pointer to pointer to section struct.
+ *
+ * @note s have to be pointing to NULL. ( *s == NULL ). This structure is deallocated
+ * with calling free_object_file on the object where this section is.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int new_section(char *secion_name, section_t **s);
+
+/**
+ * @brief Create new special symbol.
+ *
+ * @param name Name of the symbol.
+ * @param value Value of the symbol.
+ * @param type Type of the symbol, export or import. See symbol_type_t.
+ * @param s Pointer to pointer to struct where symbol will be created.
+ *
+ * @note s have to be pointing to NULL. ( *s == NULL ). This structure is deallocated
+ * with calling free_object_file on the object where this section is.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int new_spec_symbol(char *name, uint32_t value, symbol_type_t type, spec_symbol_t **s);
+
+/**
+ * @brief Create new data symbl.
+ *
+ * @param address Address where this symbol will be stored relative to the beggining of section.
+ * @param type Type of the data symbol, can be instruction or blob see data_symbol_type_t.
+ * @param payload_ptr Pointer (void *), that will point to the structure, it can be instance of structure from isa, or datablob_t.
+ * @param d Pointer to pointer to struct where symbol will be created.
+ *
+ * @note d have to be pointing to NULL. ( *d == NULL ). This structure is deallocated
+ * with calling free_object_file on the object where this section is.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int new_data_symbol(uint32_t address, data_symbol_type_t type, void *payload_ptr, data_symbol_t **d);
+
+/**
+ * @brief Create new instance of datablob_struct.
+ *
+ * @param lenght Bytes needed for payload.
+ * @param b Pointer to pointer to struct where blob will be created.
+ *
+ * @return 0 if ok, -1 if fail
+ *
+ * @note b have to be pointing to NULL. ( *b == NULL ). This structure is deallocated
+ * with calling free_object_file on the object where this section is.
+ *
+ * This function dosn't fill your blob with any data, you have to do it at yourself,
+ * see datablob_t for details about structure members.
+ */
 int new_blob(unsigned int lenght, datablob_t **b);
 
-//přidává do objektového souboru či do sekce
+/**
+ * @brief Append section into object file.
+ *
+ * This function will connect section s into double linked list in object o.
+ *
+ * @param o Object file structure.
+ * @param s Section to connect.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int append_section_to_obj(obj_file_t *o, section_t *s);
+
+/**
+ * @brief Append special symbol into section.
+ *
+ * This function connect special symbol at the end of the double linked list in the section.
+ *
+ * @param section Section to insert special symbol.
+ * @param symbol Symbol to insert.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int append_spec_symbol_to_section(section_t *section, spec_symbol_t *symbol);
+
+/**
+ * @brief Append data symbol to section.
+ *
+ * This function connect data symbol at the end of the double linked list in the section.
+ *
+ * @param section Section to insert data symbol.
+ * @param data Data symbol that will be inserted.
+ *
+ * @return 0 if ok, -1 if fail
+ */
 int append_data_symbol_to_section(section_t *section, data_symbol_t *data);
 
 /**
