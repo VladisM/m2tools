25a41ebbeb01b334325fca27195997910aac1005
Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
2019-06-15 11:46:43 +0200

    Add base of linker

---
+++ linker/linker.c
@@ -0,0 +1,15 @@
+/**
+ * @file linker.c
+ *
+ * @brief Tool for linking object files together.
+ *
+ * @author Bc. Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("This is useless yet!\n");
+    return 1;
+}
