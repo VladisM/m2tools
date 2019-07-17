f6a5c792c05ccf7dd01b14b00caabe5962c03dc4
Bc. Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
2019-07-18 00:56:28 +0200

    update

--- linker/ldparser.c
+++ linker/ldparser.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <isa.h>
 
@@ -11,10 +12,119 @@ static inline lds_t *new_lds(void);
 static inline mem_t *new_mem(char *name, isa_address_t size, isa_address_t orig);
 static inline sym_t *new_sym(char *name, isa_address_t value);
 static inline void check_malloc(void *p);
+static inline void parse_tokens(char *t);
+
+typedef enum{
+    PS_IDLE = 0,
+    PS_COMMENT,
+    PS_TOK
+}lds_parser_state_t;
 
 lds_t *parse_lds(char *path){
-    //TODO: add parsing script
-    return NULL;
+    FILE *f = fopen(path, "r");
+    int c;
+    lds_parser_state_t parser_state = PS_IDLE;
+    unsigned int line_num = 1;
+    char *tok = NULL;
+    unsigned int tok_size_real = 2;
+    unsigned int tok_size_used = 0;
+    lds_t *my_lds = new_lds();
+
+    tok = malloc(sizeof(char) * tok_size_real);
+    check_malloc((void *)tok);
+
+    memset((void *)tok, 0, sizeof(char) * tok_size_real);
+
+    if(f == NULL){
+        fprintf(stderr, "Cannot open linker script file '%s'.\n", path);
+        free(tok);
+        free_lds(my_lds);
+        exit(EXIT_FAILURE);
+    }
+
+    while((c = fgetc(f)) != EOF){
+        if(c == '\r') continue;
+        if(c == '\t') c = ' ';
+        if(c == '\n') line_num++;
+
+        switch(parser_state){
+            case PS_IDLE:
+                if(c == ';'){
+                    parser_state = PS_COMMENT;
+                }
+                else if(c == ' '){
+                    parser_state = PS_IDLE;
+                }
+                else if(isalnum(c) || c == '.' || c == '_'){
+                    goto care_about_token;
+                }
+                else{
+                    fprintf(stderr, "Syntax err in linker script file at line %d.\n", line_num);
+                    fclose(f);
+                    free(tok);
+                    free_lds(my_lds);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case PS_TOK:
+                if(c == '\n' || c == ' '){
+                    parser_state = PS_IDLE;
+
+                    if(tok_size_used == tok_size_real){
+                        char *new_tok = realloc(tok, sizeof(char) * tok_size_real * 2);
+                        check_malloc(new_tok);
+                        tok = new_tok;
+                        tok_size_real *= 2;
+                    }
+                    tok[tok_size_used] = '\0';
+
+                    parse_tokens(tok);
+
+                    memset((void *)tok, 0, sizeof(char) * tok_size_real);
+                    tok_size_used = 0;
+                }
+                else if(isalnum(c) || c == '.' || c == '_'){
+care_about_token:
+                    parser_state = PS_TOK;
+
+                    if(tok_size_used == tok_size_real){
+                        char *new_tok = realloc(tok, sizeof(char) * tok_size_real * 2);
+                        check_malloc(new_tok);
+                        tok = new_tok;
+                        tok_size_real *= 2;
+                    }
+                    tok[tok_size_used++] = (char)c;
+                }
+                else{
+                    fprintf(stderr, "Syntax error in linker script at line %d.\n", line_num);
+                    fclose(f);
+                    free(tok);
+                    free_lds(my_lds);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case PS_COMMENT:
+                if(c == '\n'){
+                    parser_state = PS_IDLE;
+                }
+                else{
+                    parser_state = PS_COMMENT;
+                }
+                break;
+            default:
+                fprintf(stderr, "Unknown parser state! Internal error!\n");
+                fclose(f);
+                free(tok);
+                free_lds(my_lds);
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+
+    fclose(f);
+    free(tok);
+
+    return my_lds;
 }
 
 void free_lds(lds_t *l){
@@ -112,6 +222,11 @@ static inline void check_malloc(void *p){
         fprintf(stderr, "Malloc failed!\n");
         exit(EXIT_FAILURE);
     }
+}
+
+static inline void parse_tokens(char *t){
+    printf("%s\n", t);
+    //TODO: finish parsing tokens
 }
 
 #ifndef NDEBUG
